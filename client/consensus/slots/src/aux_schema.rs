33446c9446b345c298892fc79f2b4751058a2ff8
André Silva <andre.beat@gmail.com>
2020-06-11 18:03:28 +0100

    babe: working equivocation reporting

--- client/consensus/slots/src/aux_schema.rs
+++ client/consensus/slots/src/aux_schema.rs
@@ -90,7 +90,7 @@ pub fn check_equivocation<C, H, P>(
 			// 2) with different hash
 			if header.hash() != prev_header.hash() {
 				return Ok(Some(EquivocationProof {
-					slot, // 3) and mentioning the same slot.
+					offender: signer.clone(),
 					first_header: prev_header.clone(),
 					second_header: header.clone(),
 				}));
